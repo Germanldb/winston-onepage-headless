c03e9342aab84c2b266c4a824fb472cc9188435c
itauditorspr <itauditorspr>
2026-02-02 14:19:05 -0400

    Refinamiento de header, menu mobile estilo LV, guia de tallas dinamica y mejoras de UX

---
+++ winston-onepage-headless/astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
+  image: {
+    domains: ["winstonandharrystore.com"],
+  },
+});
